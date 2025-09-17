db2a7be623131012791d074f108a3eb22a8dacf3
jwallvtgc-lang <jwallvtgc@gmail.com>
2025-09-17 17:51:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,42 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     include: [
-      "@tensorflow-models/pose-detection",
-      "@tensorflow/tfjs",
-      "@tensorflow/tfjs-backend-webgl"
-    ]
+      '@tensorflow/tfjs',
+      '@tensorflow/tfjs-backend-webgl',
+      '@tensorflow/tfjs-converter',
+      '@tensorflow/tfjs-core',
+      '@tensorflow-models/pose-detection',
+      '@mediapipe/pose'
+    ],
+    exclude: []
   },
-  ssr: {
-    noExternal: [
-      "@tensorflow-models/pose-detection",
-      "@tensorflow/tfjs",
-      "@tensorflow/tfjs-backend-webgl"
-    ]
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          tensorflow: [
+            '@tensorflow/tfjs',
+            '@tensorflow/tfjs-backend-webgl',
+            '@tensorflow/tfjs-converter',
+            '@tensorflow/tfjs-core',
+            '@tensorflow-models/pose-detection'
+          ]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  },
+  define: {
+    global: 'globalThis',
   }
-});
-
-
+})
