0a65c82a3a4d2887e30ed72b359b9bb14c95da77
jwallvtgc-lang <jwallvtgc@gmail.com>
2025-09-18 20:28:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,47 +5,48 @@ import { componentTagger } from "lovable-tagger"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  optimizeDeps: {
-    include: [
-      '@tensorflow/tfjs',
-      '@tensorflow/tfjs-backend-webgl',
-      '@tensorflow/tfjs-converter',
-      '@tensorflow/tfjs-core',
-      '@tensorflow-models/pose-detection'
-    ]
-  },
-  build: {
-    target: 'esnext',
-    sourcemap: false,
-    rollupOptions: {
-      external: ['@tensorflow/tfjs-backend-webgpu'],
-      output: {
-        manualChunks: {
-          tensorflow: [
-            '@tensorflow/tfjs',
-            '@tensorflow/tfjs-backend-webgl',
-            '@tensorflow/tfjs-converter',
-            '@tensorflow/tfjs-core',
-            '@tensorflow-models/pose-detection'
-          ]
-        }
-      }
-    }
-  },
-  define: {
-    global: 'globalThis',
-  },
+  base: '/',
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  optimizeDeps: {
+    include: [
+      '@tensorflow/tfjs',
+      '@tensorflow/tfjs-backend-webgl',
+      '@tensorflow/tfjs-converter',
+      '@tensorflow/tfjs-core',
+      '@tensorflow-models/pose-detection'
+    ]
+  },
+  build: {
+    target: 'esnext',
+    sourcemap: false,
+    rollupOptions: {
+      external: ['@tensorflow/tfjs-backend-webgpu'],
+      output: {
+        manualChunks: {
+          tensorflow: [
+            '@tensorflow/tfjs',
+            '@tensorflow/tfjs-backend-webgl',
+            '@tensorflow/tfjs-converter',
+            '@tensorflow/tfjs-core',
+            '@tensorflow-models/pose-detection'
+          ]
+        }
+      }
+    }
+  },
+  define: {
+    global: 'globalThis',
+  },
 }))
